b24bf8a2efdf7db29bf61e33717781d944167f59
nimondo <znimed@gmail.com>
2021-08-10 11:22:07 +0000

    Add auth middlware

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -27,7 +28,11 @@ User.findOne({ email: req.body.email })
         }
         res.status(200).json({
             userId: user._id,
-            token: 'TOKEN'
+            token: jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+              )
         });
         })
         .catch(error => res.status(500).json({ error }));
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../middleware/auth');
 const productCtrl = require('../controllers/product');
 
 //post
 router.post('/', productCtrl.createProduct);  
 
-router.get('/', productCtrl.getProducts);
+router.get('/', auth, productCtrl.getProducts);
 router.get('/:id', productCtrl.getOneProduct);  
 router.put('/:id', productCtrl.updateProduct);
 router.delete('/:id', productCtrl.deleteProduct);
